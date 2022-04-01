4de64deef7f978cbf741b0e76f0306115e70f7f5
EgemenCiftci <egemenciftci@gmail.com>
2022-04-01 16:22:02 +0200

    5 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  num = 5;
-
-  increment() {
-    this.num++;
-    console.log('increment from parent!');
-  }
-}
+export class AppComponent {}
